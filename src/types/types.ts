80f41f84f9a8cc6682fa41acd21ac0f62ed1d0d1
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac.fritz.box>
2026-01-30 14:30:51 +0100

    Creazione Interface primarie

---
+++ src/types/types.ts
@@ -0,0 +1,28 @@
+export interface IPartecipante {
+    nome: string;
+    cognome: string;
+    paeseDiOrigine: string;
+    livelloDiIstruzione: string[];
+    competenzeLinguistiche: string[];
+    ambitoDiFormazione: string;
+
+    iscrivitiCorso(corso: Icorso): void;
+}
+export interface ICorso {
+    titoloDelCorso: string;
+    descrizione: string;
+    settoreProfessionale: string;
+    durata: number;
+    elencoIscritti: IPartecipante[];
+
+    aggiungiPartecipante(partecipante: IPartecipante): void;
+}
+
+export interface IAzienda{
+    nomeAzienda: string;
+    settoreAttività: string;
+    descrizione: string;
+    posizioniAperte: string[];
+
+    offriPosizione(partecipante: IPartecipante, posizione: string): void;
+}
